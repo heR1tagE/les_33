1c1cc2cd8acf010e56d16b3247ec14b2c1921a53
heR1tagE <vanya200462@gmail.com>
2024-09-18 18:05:13 +0300

    Оновлено gulpfile.js для підтримки autoprefixer та динамічного імпорту

--- gulpfile.js
+++ gulpfile.js
@@ -3,20 +3,17 @@ const sass = require('gulp-sass')(require('sass'));
 const cleanCSS = require('gulp-clean-css');
 const browserSync = require('browser-sync').create();
 
-// Динамічний імпорт autoprefixer
 async function loadAutoprefixer() {
   const autoprefixer = (await import('gulp-autoprefixer')).default;
   return autoprefixer;
 }
 
-// Шляхи до файлів
 const paths = {
   scss: './src/scss/**/*.scss',
   css: './dist/css',
   html: './src/*.html',
 };
 
-// Завдання для компіляції SCSS у CSS
 async function styles() {
   const autoprefixer = await loadAutoprefixer();
   
@@ -31,13 +28,11 @@ async function styles() {
     .pipe(browserSync.stream());
 }
 
-// Завдання для перезавантаження браузера
 function reload(done) {
   browserSync.reload();
   done();
 }
 
-// Налаштування BrowserSync
 function serve() {
   browserSync.init({
     server: './src',
@@ -47,6 +42,5 @@ function serve() {
   gulp.watch(paths.html, reload);
 }
 
-// Створення основних завдань
 exports.styles = styles;
 exports.serve = gulp.series(styles, serve);
